48d4fc6fb18eed781ad2a3ce860ed1cb92392a6e
Ken Barlow <kenbrlow@gmail.com>
2021-01-06 09:46:01 -0700

    Built out handler.rb and serverless.yml

--- germanizer/handler.rb
+++ germanizer/handler.rb
@@ -1,11 +1,18 @@
 require 'json'
+require 'telegram/bot'
+require 'aws-sdk-translate'
 
-def hello(event:, context:)
-  {
-    statusCode: 200,
-    body: {
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event
-    }.to_json
-  }
+def translate(event:, context:)
+  
+  client = Aws:Translate::Client.new
+  bot = Telegram::Bot::Client.new(ENV["TG_TOKEN"])
+  message = JSON.parse(event["body"])["message"]
+
+  translation = client.translate_text({
+    text: message["text"],
+    source_language_code: "auto",
+    target_languge_code: "DE"
+  }).translated_text
+
+  bot.api.send_message(chat_id: message["chat"]["id"], text: translation)
 end
